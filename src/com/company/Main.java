3074cb62dacec25b034d06d5f365a2e8b90704d0
chibatuz <chibatuz@gmail.com>
2020-12-05 17:07:04 +0400

    New commit_01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,25 +14,30 @@ public class Main {
 
         if (one > two) {
             System.out.println("Min number is: " + two);
-        }else {
+        } else if (one == two) {
+            System.out.println("The values are equal");
+        } else {
             System.out.println("Min number is: " + one);
         }
+
 
         // number description
         int number = -12;
 
-        if (number > 0) {
-            System.out.println(-12 + " Is above zero");
-        } if (number < 0) {
-            System.out.println(-12 + " Is below zero");
-        } if (number %2 == 0) {
-            System.out.println(-12 + " Is even number");
+        if (number > 0 & number % 2 == 0) {
+            System.out.println(number + " is above zero and even number");
+        } else if (number > 0 & number % 2 == 1) {
+            System.out.println(number + " is above zero and is an odd number");
+        } else if (number == 0) {
+            System.out.println(number + " is zero");
+        } else if (number < 0 & number % 2 == 0) {
+            System.out.println(number + " is below zero and even number");
         } else {
-            System.out.println(-12 + " Is an odd number");
+            System.out.println(number + " is below zero and is an odd number");
         }
 
         // multiplication table
-        for (int i = 1; i <= 10; i++){
+        for (int i = 1; i <= 10; i++) {
             System.out.println(("1x" + i + " =  " + i + "\t" + "2x" + i + " =  " + (2 * i)
                     + "\t" + "3x" + i + " =  " + (3 * i) + "\t" + "4x" + i + " =  " + (4 * i)
                     + "\t" + "5x" + i + " =  " + (5 * i) + "\t" + "6x" + i + " =  " + (6 * i)
@@ -41,13 +46,28 @@ public class Main {
         }
 
         // arithmetic progression for N
-        for (int i = 1; i <= 10; i++){
-            System.out.println(i);
+        System.out.println("arithmetic progression for N");
+        int n = 10; // num of elements
+        int a = 1; // start point
+        int d = 1; // factor
+
+        int[] ari_array = new int[n];
+        for (int i = 0; i < n; i++) {
+            ari_array[i] = a + d * i;
+            System.out.print(ari_array[i] + " ");
         }
 
         // geometric progression for N
-        for (int i = 1; i <= 1000; i *= 2){
-            System.out.println(i);
+        System.out.println(" ");
+        System.out.println("geometric progression for N");
+        int n_01 = 1000; // num of elements
+        int a_01 = 1; // start point
+        int d_01 = 1; // factor
+
+        int[] geo_array = new int[n_01];
+        for (int y = 1; y < n_01; y *= 2) {
+            geo_array[y] = a_01 * d_01 * y;
+            System.out.print(geo_array[y] + " ");
         }
     }
 }
